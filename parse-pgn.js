fedd41756c233935ef3a07741a383f26c16f9666
Kevin Ludwig <kevin.j.ludwig.-nd@disney.com>
2017-06-22 13:14:07 -0700

    get rid of unnecessary new

--- parse-pgn.js
+++ parse-pgn.js
@@ -2,6 +2,6 @@
 const fs = require('fs'),
       pegjs = require('pegjs');
 
-const parser = new pegjs.generate(fs.readFileSync('./grammar.peg', 'utf-8'));
+const parser = pegjs.generate(fs.readFileSync('./grammar.peg', 'utf-8'));
 const result = parser.parse(fs.readFileSync(process.argv[2], 'utf-8'));
 console.log(JSON.stringify(result, null, 2));
