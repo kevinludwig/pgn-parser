87d0e1f9f3172de5afed469ea3e19c94027e09ca
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-06-23 19:07:55 -0700

    fix some issues with package

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs'),
+      path = require('path'),
       pegjs = require('pegjs');
 
 module.exports = function(callback) {
-    fs.readFile('./grammar.peg', 'utf-8', function(err, data) {
+    fs.readFile(path.join(__dirname, 'grammar.peg'), 'utf-8', function(err, data) {
         if (err) {
             callback(err);
         } else {
