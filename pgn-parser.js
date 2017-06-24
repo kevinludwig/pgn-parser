edcb9eae0cf039c68beba5c9e734b1859260385e
Kevin Ludwig <kevin.ludwig@gmail.com>
2017-06-23 19:16:03 -0700

    add error handling; exit with 1 on error

--- pgn-parser.js
+++ pgn-parser.js
@@ -3,7 +3,17 @@ const fs = require('fs'),
       pgnParser = require('./index');
 
 pgnParser((err, parser) => {
-    fs.readFile(process.argv[2], 'utf-8', (err, data) => {
-        console.log(JSON.stringify(parser.parse(data), null, 2));
-    });
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    } else {
+        fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+                process.exit(1);
+            } else {
+                console.log(JSON.stringify(parser.parse(data), null, 2));
+            }
+        });
+    }
 });
