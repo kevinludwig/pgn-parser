3c74401e30302edf13a7707b0fa5800b6a8b435a
Kevin Ludwig <kevin.ludwig@gmail.com>
2020-05-15 07:52:19 -0700

    headers as array, support multiple comments between half move

--- test/test_grammar.js
+++ test/test_grammar.js
@@ -137,6 +137,11 @@ describe('grammar', () => {
     it('should allow single line comments', () => {
         const results = parser.parse('; lead comment\n[SomeHeader "Value"]\n\n1. e4 e5 *');
         results.should.have.lengthOf(1);
-        results[0].comment_above_header.should.have.lengthOf(1);
+        results[0].comments_above_header.should.have.lengthOf(1);
+    });
+
+    it('should support multiple comments between half-moves', () => {
+        const results = parser.parse('1. e4 { this is a comment } { [%csl Gd5,Gf5][%cal Ge4d5,Ge4f5] } *');
+        results[0].moves.should.have.lengthOf(1);
     });
 });
