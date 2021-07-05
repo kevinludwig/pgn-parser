786efa45881f097e1819cd293b70fedd305dcdc2
Tyler Neisinger <tjneisi@gmail.com>
2021-07-05 13:16:30 -0700

    Parse comments found at start of RAVs

--- test/test_grammar.js
+++ test/test_grammar.js
@@ -90,6 +90,11 @@ describe('grammar', () => {
         result.moves.should.have.lengthOf(4);
     });
 
+    it('should allow RAV with comments', () => {
+        const [result] = parser.parse('1. e4 ({c1} {c2} 1. d4 d5) e5 2. d4 exd4 *');
+        result.moves.should.have.lengthOf(4);
+    });
+
     it('should allow RAV with no trailing whitespace', () => {
         const [result] = parser.parse('1. e4 (1. d4 d5) e5 2. d4 exd4 *');
         result.moves.should.have.lengthOf(4);
@@ -104,7 +109,7 @@ describe('grammar', () => {
         const [result] = parser.parse('1. e4 (1. d4 d5)(1. f4 e5)e5 2. d4 exd4 *');
         result.moves.should.have.lengthOf(4);
     });
-    
+
     it('should allow promotion without equal sign', () => {
         const [result] = parser.parse('1. e4 f5 2. exf5 g6 3. fxg6 h6 4. g7 Rh7 5. gxf8Q+ *');
         result.moves.should.have.lengthOf(9);
